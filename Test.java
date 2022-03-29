c373f4c3afacac02d8ff49b86fbf6740bb7a80f8
hashmmaheaalam <haalam@dminc.com>
2022-03-29 21:40:17 +0530

    Intance varable and instance area concept

--- Test.java
+++ Test.java
@@ -1,4 +1,5 @@
 public class Test {
+	int a=25,b=45,c=11;    // instance variables:- The variable defined inside the class(instance area) and outside of method/constructor/block are called instance variable
 	public static void main(String[] args){
 		int a=10,b=20;   // local variables:- The variable defined inside the block, method and in constructor are called local variables
 		System.out.println(a+b);
@@ -7,6 +8,8 @@ public class Test {
 	void sampleMethod(){
 		int a=80;
 		System.out.println(a);      //Scope :- output will be 80 i.e main method local variable a is not visible to sampleMethod
-		//System.out.println(b);   // This line will give error:- can not find variable b
+		System.out.println(b);   // This line will not give error i.e. if JVM not find b in local area it goes to instance area
+		int c=50;
+		System.out.println(c);    //JVM  finds c in local area so it will not go to instance area i.e. local variable has first priority
 	}
 }
