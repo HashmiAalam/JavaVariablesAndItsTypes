398ec80a14a2ee1bbe6b36c0ee8ae4268f82d7f4
hashmmaheaalam <haalam@dminc.com>
2022-03-30 15:26:37 +0530

    Program on instance variable:-call two instance variable from static method

--- Test.java
+++ Test.java
@@ -1,26 +1,18 @@
+//call two instance variable from static method
 public class Test {
-	int a=25,b=45,c=11;    // instance variables:- The variable defined inside the class(instance area) and outside of method/constructor/block are called instance variable
-	static int d=40,e=70;
+	int a=25,b=45;     // instance area-instance variables
 	public static void main(String[] args){    //static method
-	      //static area
-		int a=10,b=20;   // local variables:- The variable defined inside the block, method and in constructor are called local variables
-		System.out.println(a+b);
-		Test t=new Test();   // object creation
-        t.sampleMethod();   //JVM is responsible for execution of main method only if we don't call sampleMethod then JVM will not call it by itself
-		                    // to call user defined method object reference is needed 't' in this case
-        System.out.println(t.a);  //Different area(static-instance) reference is required to access instance variables 
-        //we can call static variables directly
-        System.out.println(d+e);
-        // but it is recommended to call it by class name
-          System.out.println(Test.e-Test.d);		
+	     Test.sampleStaticMethod1();      
+		 sampleStaticMethod2();   //we can call static method(in same package) directly but it is recommondet to call static method via class name
 	}
-	void sampleMethod(){
-		int a=80;
-		System.out.println(a);      //Scope :- output will be 80 i.e main method local variable a is not visible to sampleMethod
-		System.out.println(b);   //since JVM not find b in local area it goes to instance area, No reference is needed because area is same(instance-instance) 
-		int c=50;
-		System.out.println(c);    //JVM  finds c in local area so it will not go to instance area i.e. local variable has first priority
-		System.out.println(Test.d);  //we can access static variables via class name
-		System.out.println(Test.e);
+	static void sampleStaticMethod1(){            //static method
+		System.out.println(new Test().a);   //static-area and instance-variable:- so object creation is needed because a and b get memory when object is created
+		System.out.println(new Test().b);     // System.out.println(b); if we call directly we get error:- non-static variable b cannot be referenced from a static context 
+
+	}
+	static void sampleStaticMethod2(){          //static method
+		System.out.println(new Test().a);
+		System.out.println(new Test().b);
+
 	}
 }
