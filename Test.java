ea0d7c9a021a2c737d10369fbc2a759e9d482ab8
hashmmaheaalam <haalam@dminc.com>
2022-03-30 12:25:09 +0530

    Different areas in java code like static area and instance area in this code I tried to explaine about them

--- Test.java
+++ Test.java
@@ -1,14 +1,17 @@
 public class Test {
 	int a=25,b=45,c=11;    // instance variables:- The variable defined inside the class(instance area) and outside of method/constructor/block are called instance variable
-	public static void main(String[] args){
+	public static void main(String[] args){    //static method
+	      //static area
 		int a=10,b=20;   // local variables:- The variable defined inside the block, method and in constructor are called local variables
 		System.out.println(a+b);
-        new Test().sampleMethod();		
+		Test t=new Test();
+        t.sampleMethod();
+        System.out.println(t.a);  //Different area(static-instance) reference is required to access instance variables   		
 	}
 	void sampleMethod(){
 		int a=80;
 		System.out.println(a);      //Scope :- output will be 80 i.e main method local variable a is not visible to sampleMethod
-		System.out.println(b);   // This line will not give error i.e. if JVM not find b in local area it goes to instance area
+		System.out.println(b);   //since JVM not find b in local area it goes to instance area, No reference is needed because area is same(instance-instance) 
 		int c=50;
 		System.out.println(c);    //JVM  finds c in local area so it will not go to instance area i.e. local variable has first priority
 	}
