409270b62c1927341fa98d82d28821541dc4bcb6
hashmmaheaalam <haalam@dminc.com>
2022-03-30 17:27:12 +0530

    Program on static and instance variable and there accessblity in java class

--- Test.java
+++ Test.java
@@ -1,18 +1,21 @@
 //call two instance variable from static method
 public class Test {
-	int a=25,b=45;     // instance area-instance variables
+	int a=25,b=45;     // instance variables
+	static double c=30,d=50;
 	public static void main(String[] args){    //static method
-	     Test.sampleStaticMethod1();      
-		 sampleStaticMethod2();   //we can call static method(in same package) directly but it is recommondet to call static method via class name
+	     Test.sampleStaticMethod(); //we can call static method(in same package) directly but it is recommondet to call static method via class name     
+		 new Test().sampleInstanceMethod();   
 	}
-	static void sampleStaticMethod1(){            //static method
-		System.out.println(new Test().a);   //static-area and instance-variable:- so object creation is needed because a and b get memory when object is created
-		System.out.println(new Test().b);     // System.out.println(b); if we call directly we get error:- non-static variable b cannot be referenced from a static context 
-
+	void sampleInstanceMethod(){            //Instance method
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(Test.c);
+		System.out.println(Test.d);
 	}
-	static void sampleStaticMethod2(){          //static method
-		System.out.println(new Test().a);
-		System.out.println(new Test().b);
-
+	static void sampleStaticMethod(){          //static method
+	System.out.println(new Test().a);
+	System.out.println(new Test().b);
+	System.out.println(Test.c);
+	System.out.println(Test.d);
 	}
 }
