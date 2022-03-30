b695837538e470b87a44c461855430676d7ee48e
hashmmaheaalam <haalam@dminc.com>
2022-03-30 18:23:24 +0530

    Use of static and intance variable:- one memory for static variable and all objects has common accessblity to it and new memory is allocated to instance variable for each object creation

--- Test.java
+++ Test.java
@@ -1,21 +1,30 @@
-//call two instance variable from static method
+//Use of Static and Instance variables 
 public class Test {
-	int a=25,b=45;     // instance variables
-	static double c=30,d=50;
+	int a=25;     // instance variables
+	static double c=30;
 	public static void main(String[] args){    //static method
-	     Test.sampleStaticMethod(); //we can call static method(in same package) directly but it is recommondet to call static method via class name     
-		 new Test().sampleInstanceMethod();   
+	   Test t1=new Test();                       //1st object
+        	System.out.println(t1.a);  //25      //intial value
+            System.out.println(Test.c);	// 30.0
+             t1.a=t1.a+5;
+             Test.c=Test.c+5;
+ 			System.out.println(t1.a);  //30
+            System.out.println(Test.c); //35.0
+			t1.a=t1.a+5;
+             Test.c=Test.c+5;
+			 System.out.println(t1.a);  // 35
+            System.out.println(Test.c); //40.0
+			Test t2=new Test();                     //2nd object
+			System.out.println(t2.a);    //25         //intial value
+            System.out.println(Test.c);   //40.0  if one object changes the value of static variable the change will be reflected for all object  
+			t2.a=t2.a+15;
+			Test.c=Test.c+15;
+			System.out.println(t2.a);    //40
+            System.out.println(Test.c);  //55.0  
+			Test t3= new Test();                 //3rd object
+			System.out.println(t3.a);    //25    //intial value i.e. for every object new memory is allocated to instance variable
+            System.out.println(Test.c);  //55.0   if one object changes the value of static variable the change will be reflected for all object
+			                             //this means for static variables one memory is allocated and all objects has access to common memory
 	}
-	void sampleInstanceMethod(){            //Instance method
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(Test.c);
-		System.out.println(Test.d);
-	}
-	static void sampleStaticMethod(){          //static method
-	System.out.println(new Test().a);
-	System.out.println(new Test().b);
-	System.out.println(Test.c);
-	System.out.println(Test.d);
-	}
+	
 }
