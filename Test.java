f146579b9e2cc573a63aae4a8d577a620d4b2625
hashmmaheaalam <haalam@dminc.com>
2022-03-31 12:01:22 +0530

    Whenever common value is needed use static variable and whenever new value is needed go for instance variable

--- Test.java
+++ Test.java
@@ -1,30 +1,24 @@
 //Use of Static and Instance variables 
 public class Test {
-	int a=25;     // instance variables
-	static double c=30;
 	public static void main(String[] args){    //static method
-	   Test t1=new Test();                       //1st object
-        	System.out.println(t1.a);  //25      //intial value
-            System.out.println(Test.c);	// 30.0
-             t1.a=t1.a+5;
-             Test.c=Test.c+5;
- 			System.out.println(t1.a);  //30
-            System.out.println(Test.c); //35.0
-			t1.a=t1.a+5;
-             Test.c=Test.c+5;
-			 System.out.println(t1.a);  // 35
-            System.out.println(Test.c); //40.0
-			Test t2=new Test();                     //2nd object
-			System.out.println(t2.a);    //25         //intial value
-            System.out.println(Test.c);   //40.0  if one object changes the value of static variable the change will be reflected for all object  
-			t2.a=t2.a+15;
-			Test.c=Test.c+15;
-			System.out.println(t2.a);    //40
-            System.out.println(Test.c);  //55.0  
-			Test t3= new Test();                 //3rd object
-			System.out.println(t3.a);    //25    //intial value i.e. for every object new memory is allocated to instance variable
-            System.out.println(Test.c);  //55.0   if one object changes the value of static variable the change will be reflected for all object
-			                             //this means for static variables one memory is allocated and all objects has access to common memory
+	   Employee employee1=new Employee(111,"alam","TCS");
+	  System.out.println("Employee id="+employee1.eid+ " Employee name="+employee1.ename+" Employee company="+employee1.ecompany);
+	  Employee employee2=new Employee(222,"wakeel","SBI");  //employee2 changes company name
+	  System.out.println("Employee id="+employee2.eid+ " Employee name="+employee2.ename+" Employee company="+employee2.ecompany);
+	  //since ecompany variable is static if new object(Employee) do any changes to company name than this change will reflact to all Employees
+	  System.out.println("Employee id="+employee1.eid+ " Employee name="+employee1.ename+" Employee company="+employee1.ecompany); //now employee1's company become SBI
+	  //Static variable is accessed by class name and class is one therefore memory location is same for static variable
 	}
+	
+}
+class Employee{
+	int eid;
+	String ename;
+	static String ecompany;
+	Employee(int eid,String ename,String ecompany){
+		this.eid=eid;
+		this.ename=ename;
+		this.ecompany=ecompany;
+		}
 	
 }
